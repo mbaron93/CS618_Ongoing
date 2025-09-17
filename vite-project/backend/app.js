b69f097fc35382a1433bec3e5a570abc587f0145
mbaron93 <mbaron@herronhighschool.org>
2025-09-17 23:48:32 +0000

    backend is all done including Cors

--- vite-project/backend/app.js
+++ vite-project/backend/app.js
@@ -2,11 +2,16 @@ import dotenv from 'dotenv'
 import express from 'express'
 import { postRoutes } from './src/routes/posts.js'
 import { initDB } from './src/db/initdb.js'
+import bodyParser from 'body-parser'
 
 
 dotenv.config()
 
 const app = express()
+app.use(bodyParser.json())
+app.use(cors()) //accept requests from different origins
+//cors = cross origin resource sharing
+//cors is for security reasons, browsers block requests from different origins by default
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
