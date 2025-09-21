f40edeb1c024f17a4ba0c505184ba895ab1ae157
mbaron93 <mbaron@herronhighschool.org>
2025-09-21 18:16:04 +0000

    updated with non required. email and .env error

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,27 @@
-import dotenv from 'dotenv'
-import express from 'express'
-import { postRoutes } from './src/routes/posts.js'
-import { initDB } from './src/db/initdb.js'
-import bodyParser from 'body-parser'
-import cors from 'cors'
+import dotenv from "dotenv";
+import express from "express";
+import { postRoutes } from "./src/routes/posts.js";
+import { initDB } from "./src/db/initdb.js";
+import bodyParser from "body-parser";
+import cors from "cors";
 
+try {
+  require("dotenv").config();
+} catch (err) {
+  console.log("No .env file found, using environment variables");
+}
 
-dotenv.config()
-
-const app = express()
-app.use(bodyParser.json())
-app.use(cors()) //accept requests from different origins
+const app = express();
+app.use(bodyParser.json());
+app.use(cors()); //accept requests from different origins
 //cors = cross origin resource sharing
 //cors is for security reasons, browsers block requests from different origins by default
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-await initDB()
-postRoutes(app)
+await initDB();
+postRoutes(app);
 
-export {app}
+export { app };
